62ce25f9ccc9fd318c95baa317689a321272f0f4
Marcos Daniel Torres <mdaniel.torres7@gmail.com>
2020-07-12 16:14:45 -0300

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
+    
+    //mongodb://user:password@127.0.0.1/task-manager-api?authSource=admin
     //mongodb://myTester:1234@127.0.0.1:27017/task-manager-api
 
     useNewUrlParser: true,
