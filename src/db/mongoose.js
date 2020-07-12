92c5be68acdc6dc797aeb5d6b70b60717bf02497
Marcos Daniel Torres <mdaniel.torres7@gmail.com>
2020-07-12 16:15:57 -0300

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
     
-    //mongodb://user:password@127.0.0.1/task-manager-api?authSource=admin
-    //mongodb://myTester:1234@127.0.0.1:27017/task-manager-api
+    //mongodb://127.0.0.1/task-manager-api -> desarrollo local
+    //mongodb://user:password@127.0.0.1/task-manager-api?authSource=admin -> desarrollo vps
+    //mongodb://myTester:1234@127.0.0.1:27017/task-manager-api -> example
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
